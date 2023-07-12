f2cf36123753a8e57586acf44544948656651bee
github_zulhanris <you@zulhanris.com>
2023-07-12 12:19:45 +0700

    Upload

---
+++ src/views/home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div className="p-5 mb-4 bg-light rounded-3">
+            <div className="container-fluid py-5">
+                <h1 className="display-5 fw-bold">RESPONSI TWL</h1>
+                <p className="col-md-8 fs-4">RESPONSI TWL2023</p>
+            </div>
+        </div>
+    )
+}
